8549ece31eca3f0e253bd47513815b5eeae278b0
arnokay <arnokay.work@gmail.com>
2023-12-22 23:25:50 +0200

    add comment.nvim plugin

---
+++ lua/arnokay/plugins/comment.lua
@@ -0,0 +1,14 @@
+return {
+	"numToStr/Comment.nvim",
+	config = function()
+		require("Comment").setup({
+			toggler = {
+				line = "<C-_>",
+			},
+			opleader = {
+				line = "<C-_>",
+			},
+		})
+	end,
+	lazy = false,
+}
